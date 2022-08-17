b48bcbeff9484af1cff57354a79a64c5629337fb
lukas <lukasbelay87@gmail.com>
2022-08-16 19:50:21 -0700

    added page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,48 @@
 import 'package:flutter/material.dart';
+import 'page/Home_Page.dart';
+import 'page/sus.dart';
+import 'page/Works_Page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentIndex = 0;
+  final screens = [
+    const HomePage(),
+    const WorksPage(),
+    const Sus(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.pink,
-          title: const Text("flutter is fun"),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('my app'),
-          ),
-        ),
+            backgroundColor: Colors.pink,
+            title: const Center(
+              child: Text("flutter is fun"),
+            )),
+        body: screens[currentIndex],
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() => currentIndex = index),
           items: const [
             BottomNavigationBarItem(
                 icon: Icon(Icons.access_alarm),
-                label: 'sus',
+                label: 'home',
                 backgroundColor: Colors.amber),
             BottomNavigationBarItem(
                 icon: Icon(Icons.agriculture),
-                label: 'sus',
+                label: 'works',
                 backgroundColor: Color.fromARGB(255, 143, 110, 9)),
             BottomNavigationBarItem(
                 icon: Icon(Icons.agriculture), label: 'sus'),
---
+++ lib/page/Works_Page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WorksPage extends StatelessWidget {
+  const WorksPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+        body: Center(child: Text("this is works")),
+      );
+}
